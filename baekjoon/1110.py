825aaa05c543c9a5bbdda684017f706a306be1fd
jieun <37922134+ji-eun-k@users.noreply.github.com>
2020-03-06 23:00:34 +0900

    MAR -6

--- baekjoon/1110.py
+++ baekjoon/1110.py
@@ -1,16 +1,12 @@
-n1 = n = input()
-count = new = 0
+n1 = n = int(input())
+count = newNum = 0
 
-while int(n) != int(new):
-    if int(n1) < 10:
-        n1 = '0'+n1
-    n2 = int(n1[0])+int(n1[1])
-    n2 = str(n2)
-    if int(n2) < 10:
-        n2 = '0'+n2
-    new = n1[1]+n2[1]
-    print(new)
-    n1 = new
+while True :
+    n2 = (n1//10) + (n1%10)
+    newNum = (n1%10) * 10 + n2 % 10
+    n1 = newNum
     count += 1
+    if n == newNum:
+        break
 
 print(count)
